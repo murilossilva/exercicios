1d2beded5b1d94fbd04d49949950768b786948d6
Murilo Souza da Silva <murilossilva@usp.br>
2021-09-08 08:54:36 -0300

    insere exercício 2

---
+++ Exercicio2.js
@@ -0,0 +1,15 @@
+var number = [];
+var i;
+
+for (i=0; i<10; i++){
+    number[i] = prompt("Insira um número inteiro: ")
+}
+
+var max = number.reduce(function(a, b){
+    return Math.max(a,b)
+});
+var min = number.reduce(function(a, b){
+    return Math.min(a,b)
+});
+alert("O valor máximo é" + max);
+alert("O valor mínimo é" + min);
